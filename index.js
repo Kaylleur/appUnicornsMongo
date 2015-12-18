90e4bc8706637e9038b1dd6386165dba31d9b85c
kaylleur <thomas85340@gmail.com>
2015-12-18 18:20:23 +0100

    add header to allow use of api

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ var bodyParser = require('body-parser');
 var unicornsRoute= require("./routes/unicorns_router");
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+  next();
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/unicorns', unicornsRoute);
-
 
 
 var server = app.listen(8000, function () {
